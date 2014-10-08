97c87e167ff4cb5d45e57e5a60d224e4bd1b0e0a
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-08 19:38:06 -0400

    initial routes created, no nesting

--- line-please/config/routes.rb
+++ line-please/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :lines
+  resources :characters, only: [:create, :update, :destroy]
+  resources :scenes, only: [:create, :update, :destroy]
+  resources :scripts, only: [:create, :update, :destroy]
+  resources :scene_characters, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
