3d1a0f9f02fef757bc3ce4dd69b64561e5197920
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-09 19:38:38 -0400

    intialized new scene form

--- line-please/app/controllers/scenes_controller.rb
+++ line-please/app/controllers/scenes_controller.rb
@@ -1,6 +1,11 @@
 class ScenesController < ApplicationController
 
   def index
+    @scenes = Scene.where(script_id: params[:script_id])
+  end
+
+  def new
+    @scene = Scene.new
   end
 
   def create
