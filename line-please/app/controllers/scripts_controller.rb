b83337b4dc3da4692a01eca628eb55734dff5b55
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-09 19:13:50 -0400

    add create script functionality

--- line-please/app/controllers/scripts_controller.rb
+++ line-please/app/controllers/scripts_controller.rb
@@ -5,15 +5,24 @@ class ScriptsController < ApplicationController
   end
 
   def new
+    @script = Script.new
   end
 
   def create
+    @script = Script.create(script_params)
+    redirect_to scripts_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def script_params
+    params.require(:script).permit(:title, :author)
   end
 
 end
