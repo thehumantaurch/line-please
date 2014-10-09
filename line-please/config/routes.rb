e91b88b144b0c45248d2dcee5ad21b10c1f941d2
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-09 19:58:09 -0400

    add user/sessions routing from users-sessions branch

--- line-please/config/routes.rb
+++ line-please/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :scripts, only: [:index, :new, :create]
   resources :scenes
   # The priority is based upon order of creation: first created -> highest priority.
