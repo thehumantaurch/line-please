9b8375161295ea3ac1b10b37fd091978dc6620a8
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-09 19:51:43 -0400

    fix new user form

--- line-please/app/controllers/sessions_controller.rb
+++ line-please/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to '/', :notice => "Logged in!"
     else
       flash[:notice] = "Invalid email or password"
       redirect_to new_session_path
