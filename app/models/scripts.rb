dc3931d4032626b1f86bba5b7f5d098e345cc686
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-12-15 23:20:55 -0500

    add passing tests for Script model

--- app/models/scripts.rb
+++ app/models/scripts.rb
@@ -2,4 +2,5 @@ class Scripts < ActiveRecord::Base
   has_many :scenes
   has_many :characters
   has_many :lines
+  validates :author, :title, presence: true
 end
--- spec/models/scripts_spec.rb
+++ spec/models/scripts_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 describe Scripts do
-  it "is valid with a title and author"
-  it "is invalid without an author"
-  it "is invalid without a title"
+  it "is valid with a title and author" do
+    script = Scripts.new(
+      author: 'William Shakespeare',
+      title: 'Hamlet')
+    expect(script).to be_valid
+  end
+  it "is invalid without an author" do
+    script = Scripts.new(author: nil)
+    script.valid?
+    expect(script.errors[:author]).to include("can't be blank")
+  end
+  it "is invalid without a title" do
+    script = Scripts.new(title: nil)
+    script.valid?
+    expect(script.errors[:title]).to include("can't be blank")
+  end
+
 end
