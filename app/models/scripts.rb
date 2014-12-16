fbde63394a086e6a1bfbf36d0a8a4dc8a0a77b5f
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-12-15 23:02:09 -0500

    outline script model specs

--- app/models/scripts.rb
+++ app/models/scripts.rb
@@ -1,5 +1,5 @@
 class Scripts < ActiveRecord::Base
-  has_many :scripts
+  has_many :scenes
   has_many :characters
   has_many :lines
 end
---
+++ spec/models/script_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Script do
+  it "is valid with a title and author"
+  it "is invalid without an author"
+  it "is invalid without a title"
+end
