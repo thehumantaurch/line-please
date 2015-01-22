ece88acefcb459239a53ab8889fce239f9121479
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-01-22 12:50:34 -0500

    add methods to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def post
+  end
+
+  def delete
+  end
+
 end
