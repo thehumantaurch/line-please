ae00524539137570b079453b5912cf5b549af52a
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-01-22 12:52:14 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118150126) do
+ActiveRecord::Schema.define(version: 20150122174151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 20141118150126) do
   create_table "scripts", force: true do |t|
     t.string   "title"
     t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username",                    null: false
+    t.string   "password_digest",             null: false
+    t.string   "email",                       null: false
+    t.string   "first_name",                  null: false
+    t.string   "last_name",                   null: false
+    t.integer  "access_level",    default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
   end
