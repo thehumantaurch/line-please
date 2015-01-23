78396b37ebf15f738101b72671a81836cf62b921
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-01-22 20:41:23 -0500

    add users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user, :class => 'Users' do
-    
+    username "The_Dane"
+    password "BillieShakes"
+    email "prince@denmarkcastle.com"
+    first_name "Hamlet"
+    last_name "Junior"
+    access_level 1
   end
-
 end
