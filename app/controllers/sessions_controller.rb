5778c6fcf5145dffcda131e8550dbd109a832a82
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-01-22 20:23:33 -0500

    add sessions methods for create/destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username: sessions_params[:username])
+    .try(:authenticate, sessions_params[:password])
+
+    if @user
+      session[:user_id] = @user.id
+      session[:toggle] = true
+      redirect_to expenses_path
+    else
+      flash[:login] = "Invalid Credentials"
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
+  def sessions_params
+    params.require(:session).permit(:username, :password)
+  end
+
 end
