5ec78818db91e69f3f102285f8204e875cd1e1d9
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-01-23 21:05:46 -0500

    update scripts migration to account for file upload

--- db/migrate/20141118150108_create_scripts.rb
+++ db/migrate/20141118150108_create_scripts.rb
@@ -3,6 +3,7 @@ class CreateScripts < ActiveRecord::Migration
     create_table :scripts do |t|
       t.string :title
       t.string :author
+      t.string :pdf_file
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150122174151) do
   create_table "scripts", force: true do |t|
     t.string   "title"
     t.string   "author"
+    t.string   "pdf_file"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
