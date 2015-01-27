fa56b8fd5feaa3db804deeed6b19517a9622c6cb
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-01-27 16:24:58 -0500

    simplify uploader

--- app/uploaders/pdf_file_uploader.rb
+++ app/uploaders/pdf_file_uploader.rb
@@ -1,75 +1,9 @@
-# encoding: utf-8
-
 class PdfFileUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-
-  process :convert_to_text
-
-  def convert_to_text
-    temp_dir = Rails.root.join('tmp', 'pdf-parser', 'text')
-    temp_path = temp_dir.join(filename)
-
-    FileUtils.mkdir_p(temp_dir)
-
-    PDF::Reader.open(current_path) do |pdf|
-      File.open(temp_path, 'w') do |f|
-        pdf.pages.each do |page|
-          f.puts page.text
-        end
-      end
-    end
-
-    File.unlink(current_path)
-    FileUtils.cp(temp_path, current_path)
-  end
-
-  def filename
-    super + '.txt' if original_filename.present?
-  end
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
-    %w(pdf)
-  end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
 end
