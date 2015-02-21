de2c8149a557abefd0cc79b7126a42e5ac513bdf
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-02-21 16:53:12 -0500

    update for passing test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper.rb'
 
 feature "Looking up scripts", js: true do
+  before do
+    Script.create(title: "Hamlet", author: "William Shakespeare")
+    Script.create(title: "Neverwhere", author: "Neil Gaiman")
+    Script.create(title: "Very Still & Hard To See", author: "Steve Yockey")
+    Script.create(title: "As You Like It", author: "William Shakespeare")
+  end
   scenario "finding scripts" do
     visit '/'
     fill_in "keywords", with: "hamlet"
