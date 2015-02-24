eef56e4ff38f850423520b6b8bb2cac0a8de6740
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-02-24 16:46:25 -0500

    add route to make test pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :scripts, only: [:index]
+  resources :scripts, only: [:index, :show]
   resources :scenes
   resources :characters
   resources :lines
