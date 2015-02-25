9be4bd2b785fac3409c26e7a4177e85dc20ff774
Sarah Taurchini <sarah.taurchini@gmail.com>
2015-02-25 18:27:25 -0500

    make controller tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :scripts, only: [:index, :show]
+  resources :scripts, only: [:index, :show, :create, :update, :destroy]
   resources :scenes
   resources :characters
   resources :lines
